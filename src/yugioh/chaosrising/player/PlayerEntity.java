d84be189b7010af5598fb5e9bce9cb5e72de1506
Michael Kramer <kramermp5@gmail.com>
2017-12-28 19:52:22 -0500

    Changed getImage a little Bit

--- src/yugioh/chaosrising/player/PlayerEntity.java
+++ src/yugioh/chaosrising/player/PlayerEntity.java
@@ -6,21 +6,34 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class PlayerEntity extends Entity {
 
-	Point position = new Point();
+	Point position;
 
 	public PlayerEntity() {
-
+		position = new Point(0, 0);
 	}
 
 	public Point getPosition() {
 		return this.position;
 	}
 
-	public BufferedImage getImage() throws IOException {
-		return ImageIO.read(new File("resources/TempPlayerIcon.png"));
+	public BufferedImage getImage()  {
+
+		BufferedImage test = null;
+		try {
+			test = ImageIO.read(new File("resources/TempPlayerIcon.png"));
+		} catch (FileNotFoundException ex) {
+			System.out.println("File Not Found");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(test == null) {
+			System.out.println(":setdsfsadf");
+		}
+		return test;
 	}
 }
