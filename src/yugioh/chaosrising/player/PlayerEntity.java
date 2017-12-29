a3ecdcb2b612e380b6dfffeb3d6a4f61044bea08
Michael Kramer <kramermp5@gmail.com>
2017-12-28 19:25:03 -0500

    Added Position to PlayerEntity

--- src/yugioh/chaosrising/player/PlayerEntity.java
+++ src/yugioh/chaosrising/player/PlayerEntity.java
@@ -2,9 +2,17 @@ package yugioh.chaosrising.player;
 
 import yugioh.chaosrising.utils.Entity;
 
+import java.awt.*;
+
 public class PlayerEntity extends Entity {
+
+	Point position = new Point();
 
 	public PlayerEntity() {
 
+	}
+
+	public Point getPosition() {
+		return this.position;
 	}
 }
